49b9a90ed285c71060a8d8f5e1bd921e5e880eec
AaronCediel <aaron.cediel@gmail.com>
2019-10-17 16:44:02 +0200

    Update foundation.js

--- docs/foundation.js
+++ docs/foundation.js
@@ -102,9 +102,9 @@ function constructFoundation() {
 		</div>\n\
 		\n\
 		<!-- Commands: -->\n\
-		<label for="openCurrentFilters" id="toolbarItemCustomCommands" class="toolbarButtonText">\n\
+		<div id="toolbarItemCustomCommands" class="toolbarButtonText">\n\
 			<span>Commands</span>\n\
-		</label>\n\
+		</div>\n\
 		\n\
 		<!-- Reset Default: -->\n\
 		<div id="toolbarItemResetDefault" class="toolbarButtonText">\n\
@@ -225,7 +225,8 @@ function constructFoundation() {
 <!-- --------------------------------------------------------------\n\
 |	Current Filters\n\
 ---------------------------------------------------------------- -->\n\
-<input type="checkbox" id="openCurrentFilters">\n\
+<input type="checkbox" id="checkCf">\n\
+<label class="abc123" for="checkCf">Open Current Filters</label>\n\
 <div id="currentFilters" class="modalContainer">\n\
 	<h4 id="currentFiltersHandle" class="modalDragHandle"><i id="modalDragIcon" class="material-icons faa-burst animated-hover">open_with</i>Current Filters<i id="modalRefreshButton" class="material-icons faa-spin animated-hover">refresh</i><i id="modalCloseButton" class="material-icons faa-burst animated-hover">close</i></h4>\n\
 	<div class="scrollArea">\n\
