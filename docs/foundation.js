2c768f725c509cc7fe865ef7f4617b0bb5f3aafd
AaronCediel <aaron.cediel@gmail.com>
2019-10-21 13:31:40 +0200

    Update foundation.js

--- docs/foundation.js
+++ docs/foundation.js
@@ -102,8 +102,7 @@ function constructFoundation() {
 		</div>\n\
 		\n\
 		<!-- Commands: -->\n\
-<input type="checkbox" id="openCurrentFilters">\n\
-<label id="buttonOpenCurrentFilters" for="openCurrentFilters">Open Current Filters</label>\n\
+		<label id="buttonOpenCurrentFilters" for="openCurrentFilters">Open Current Filters</label>\n\
 		<!--<label id="toolbarItemCustomCommands" for="openCurrentFilters">\n\
 			<span>Commands</span>\n\
 		</label>\n\
@@ -231,8 +230,8 @@ function constructFoundation() {
 <!-- --------------------------------------------------------------\n\
 |	Current Filters\n\
 ---------------------------------------------------------------- -->\n\
-<!--<input type="checkbox" id="openCurrentFilters">\n\
-<label id="buttonOpenCurrentFilters" for="openCurrentFilters">Open Current Filters</label>-->\n\
+<input type="checkbox" id="openCurrentFilters">\n\
+<!--<label id="buttonOpenCurrentFilters" for="openCurrentFilters">Open Current Filters</label>-->\n\
 <div id="currentFilters" class="modalContainer">\n\
 	<h4 id="currentFiltersHandle" class="modalDragHandle"><i id="modalDragIcon" class="material-icons faa-burst animated-hover">open_with</i>Current Filters<i id="modalRefreshButton" class="material-icons faa-spin animated-hover">refresh</i><i id="modalCloseButton" class="material-icons faa-burst animated-hover">close</i></h4>\n\
 	<div class="scrollArea">\n\
