a7cc77da4e9a7180317843edf4aa002549bcb0e1
AaronCediel <aaron.cediel@gmail.com>
2019-10-25 12:44:39 +0200

    Update foundation.js

--- docs/foundation.js
+++ docs/foundation.js
@@ -232,27 +232,27 @@ function constructFoundation() {
 		//console.log(foundationNavigation[fdnNavPage]);
 		console.log(foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_NAME+" -VS- "+activePage);
 		if (foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_NAME === activePage) {
-			foundationActivePageToolbarConfig = [
-				{"TOOLBAR_FILTERS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_FILTERS}
-				, {"TOOLBAR_BOOKMARKS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_BOOKMARKS}
-				, {"TOOLBAR_RELOAD": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_RELOAD}
-				, {"TOOLBAR_RESET": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_RESET}
-				, {"TOOLBAR_COMMANDS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_COMMANDS}
-				, {"TOOLBAR_CURRENT_FILTERS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_CURRENT_FILTERS}
-				, {"TOOLBAR_CURRENT_FILTERS_AUTO_PROCESSING": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_CURRENT_FILTERS_AUTO_PROCESSING}
-				, {"TOOLBAR_OPEN_TICKET": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_OPEN_TICKET}
-				, {"TOOLBAR_DATA_INTEGRITY": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_DATA_INTEGRITY}
-				, {"TOOLBAR_INFO": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_INFO}
-			];
+			foundationActivePageToolbarConfig = [{
+				"TOOLBAR_FILTERS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_FILTERS
+				, "TOOLBAR_BOOKMARKS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_BOOKMARKS
+				, "TOOLBAR_RELOAD": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_RELOAD
+				, "TOOLBAR_RESET": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_RESET
+				, "TOOLBAR_COMMANDS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_COMMANDS
+				, "TOOLBAR_CURRENT_FILTERS": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_CURRENT_FILTERS
+				, "TOOLBAR_CURRENT_FILTERS_AUTO_PROCESSING": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_CURRENT_FILTERS_AUTO_PROCESSING
+				, "TOOLBAR_OPEN_TICKET": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_OPEN_TICKET
+				, "TOOLBAR_DATA_INTEGRITY": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_DATA_INTEGRITY
+				, "TOOLBAR_INFO": foundationNavigation[fdnNavPage].fdnNavigation.TOOLBAR_INFO
+			}];
 		}
 		
-		foundationNavigationMenuPageList.push([
-			{"NAVIGATION_PAGE_ORDER": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ORDER}
-			, {"NAVIGATION_FOLDER_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_NAME}
-			, {"NAVIGATION_FOLDER_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_ICON}
-			, {"NAVIGATION_PAGE_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_NAME}
-			, {"NAVIGATION_PAGE_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ICON}
-		]);
+		foundationNavigationMenuPageList.push({
+			"NAVIGATION_PAGE_ORDER": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ORDER
+			, "NAVIGATION_FOLDER_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_NAME
+			, "NAVIGATION_FOLDER_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_ICON
+			, "NAVIGATION_PAGE_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_NAME
+			, "NAVIGATION_PAGE_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ICON
+		});
 	});
 	console.log("foundationActivePageToolbarConfig:");
 	console.log(foundationActivePageToolbarConfig);
