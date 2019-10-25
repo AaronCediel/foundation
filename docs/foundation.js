b7a51977805270f9b16394522a3c11a5f899918b
AaronCediel <aaron.cediel@gmail.com>
2019-10-25 11:02:18 +0200

    Update foundation.js

--- docs/foundation.js
+++ docs/foundation.js
@@ -208,16 +208,13 @@ function constructFoundation() {
 			foundationActivePageToolbarConfig.push([foundationNavigation[fdnNavPage].fdnNavigation]);
 		}
 		
-		var arrayItemPageName = foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_NAME;
-		foundationNavigationMenuPageList.push([{
-			arrayItemPageName: [
-				{"NAVIGATION_PAGE_ORDER": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ORDER}
-				, {"NAVIGATION_FOLDER_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_NAME}
-				, {"NAVIGATION_FOLDER_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_ICON}
-				, {"NAVIGATION_PAGE_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_NAME}
-				, {"NAVIGATION_PAGE_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ICON}
-			]
-		}]);
+		foundationNavigationMenuPageList.push([
+			{"NAVIGATION_PAGE_ORDER": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ORDER}
+			, {"NAVIGATION_FOLDER_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_NAME}
+			, {"NAVIGATION_FOLDER_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_FOLDER_ICON}
+			, {"NAVIGATION_PAGE_NAME": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_NAME}
+			, {"NAVIGATION_PAGE_ICON": foundationNavigation[fdnNavPage].fdnNavigation.NAVIGATION_PAGE_ICON}
+		]);
 	});
 	console.log("foundationActivePageToolbarConfig:");
 	console.log(foundationActivePageToolbarConfig);
